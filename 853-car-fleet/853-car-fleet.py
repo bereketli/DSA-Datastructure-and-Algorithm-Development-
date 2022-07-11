5ba827acaa69e1d858c0b16baba937472768de99
bereketli <95904361+bereketli@users.noreply.github.com>
2022-07-11 23:41:53 +0300

    Time: 1460 ms (43.39%), Space: 37.6 MB (71.90%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+       
+        stack=[]
+        positionSorted= sorted(enumerate(position), key=lambda i: i[1])
+       
+        for i, place in enumerate(positionSorted):
+            
+            time=float((target-place[1]))/float(speed[place[0]])
+            
+            
+            while(len(stack)>0 and stack[-1]<=time):
+                stack.pop()
+            stack.append(time)
+        return(len(stack))
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        
