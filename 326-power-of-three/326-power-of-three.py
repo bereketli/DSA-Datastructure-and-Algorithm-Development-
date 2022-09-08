2c91d0c7ef64f690b9da40eb7360a433b637f295
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-08 10:59:00 +0300

    Time: 83 ms (90.53%), Space: 13.6 MB (14.40%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -2,16 +2,18 @@ class Solution(object):
     def isPowerOfThree(self, n):
         if n<=0:
             return False
-        def pow_three(n):
-            if n==1:
+        def pow_three(num,divisor):
+            if num==1:
                 
                 return True
-            elif n%3==0:
-                return pow_three(n/3)
+            elif num%3==0:
+                if num<divisor:
+                    return pow_three(num,divisor/3)
+                else: return pow_three(float(num/divisor),divisor*3)
             else:
                 
                 return False
-        return pow_three(n)
+        return pow_three(n,3)
         
         
         """
