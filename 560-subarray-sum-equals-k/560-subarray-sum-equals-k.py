2cf7d44b4f4a464b3bb4b9aabeae32e88f694056
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-13 13:37:22 +0300

    Time: 354 ms (61.28%), Space: 15.9 MB (88.72%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        prefix_store={0:1}
+        prefix_sum=0
+        count=0
+        for num in nums:
+          
+            prefix_sum+=num;
+            if prefix_sum -k in prefix_store:
+                count+=prefix_store[prefix_sum-k]
+            if prefix_sum not in prefix_store:
+                prefix_store[prefix_sum]=1
+            else:
+                prefix_store[prefix_sum]+=1
+           
+           
+        
+        return count 
+                
+        
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
