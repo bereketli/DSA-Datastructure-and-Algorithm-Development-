9c4cde29dbb192ad406b2292d293136ffc1ddb71
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-14 18:09:42 +0300

    Time: 79 ms (19.35%), Space: 13.5 MB (67.10%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        stack=[]
+        output=[0]*len(prices)
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]]>=prices[i]:
+                index=stack.pop()
+                output[index]=prices[index]-prices[i]
+            stack.append(i)
+        while stack:
+            index=stack.pop()
+            output[index]=prices[index]
+        return output
+        
+        """
+        :type prices: List[int]
+        :rtype: List[int]
+        """
+        
