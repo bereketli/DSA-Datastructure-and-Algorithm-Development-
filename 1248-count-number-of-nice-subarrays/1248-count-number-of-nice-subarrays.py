4a03a27989fb09e22c7b5d660cb5f7ffd1b1272c
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-15 17:42:18 +0300

    Time: 1964 ms (6.50%), Space: 18.6 MB (82.11%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+           ans,right=0,0
+           odd_indeces=[]
+           left=-1
+            
+           
+           while right<len(nums):
+                if nums[right]%2!=0:
+                    odd_indeces.append(right)
+                if len(odd_indeces)==k:
+                   ans+=odd_indeces[0]-left
+                elif len(odd_indeces)>k:
+                      left=odd_indeces[0]
+                      odd_indeces.pop(0)
+                      ans+=odd_indeces[0]-left
+                right+=1
+           return ans
+                      
+        
+ 
