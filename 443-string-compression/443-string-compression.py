0c931b4e2002c73f9be7e11cf855614fb21b36e5
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-18 12:51:03 +0300

    Time: 78 ms (36.84%), Space: 13.8 MB (18.66%) - LeetHub

---
+++ 443-string-compression/443-string-compression.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def compress(self, chars):
+        left,count=0,0
+        for right in range(len(chars)):
+           
+            if chars[left]==chars[right]:
+                count+=1
+            else:
+                left+=1
+                if count==1:
+                    chars[left]=chars[right]
+                    continue
+                else:
+                    for char in str(count):
+                        chars[left]=char
+                        left+=1
+                    count=1
+                    chars[left]=chars[right]
+        if count>1:
+          for char in str(count):
+                 left+=1
+                 chars[left]=char
+                       
+        
+        return left+1
+                
+        
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        
