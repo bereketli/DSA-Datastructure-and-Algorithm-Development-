acb503da44e2c788a3e7af122636311df86f6c61
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-09-27 16:25:56 +0300

    Time: 1272 ms (11.11%), Space: 29.7 MB (42.36%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,19 +1,14 @@
 class Solution(object):
     def minSetSize(self, arr):
         counted = Counter(arr)
-        ordered = sorted(counted, key = counted.get)
+        ordered = sorted(counted, key = counted.get, reverse = True)
         count , store= 0,0
         n=ceil(len (arr) /2)
-        i= len(ordered)-1
+        i= 0
         
         while store < n:
             count +=1
             store += counted[ordered[i]]
-            i -=1
+            i +=1
         return count
-            
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
-        
+    
