f43f1746530ea3348254cb00be0b6a4e72ae3685
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-02 09:43:03 +0300

    Time: 515 ms (57.89%), Space: 15.8 MB (84.21%) - LeetHub

---
+++ 475-heaters/475-heaters.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findRadius(self, houses, heaters):
+        houses.sort()
+        heaters.sort()
+        radius =0
+        h_p =0
+        for house in houses:
+            while h_p < len(heaters)-1 and house >= heaters[h_p+ 1]:
+                h_p +=1
+              
+            if h_p < len(heaters)-1:
+                radius = max(radius,min(abs(house - heaters[h_p]),abs(house - heaters[h_p +1 ])))
+            else: 
+                radius =max( radius,abs(house - heaters[h_p]) )
+        return radius
+            
+        
+        """
+        :type houses: List[int]
+        :type heaters: List[int]
+        :rtype: int
+        """
+        
