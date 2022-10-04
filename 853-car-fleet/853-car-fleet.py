e4508cd12cdf15ee73a2f36949c14bbd9499718d
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-04 20:38:26 +0300

    Time: 2533 ms (8.96%), Space: 38.9 MB (48.46%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -3,13 +3,13 @@ class Solution(object):
        
         stack=[]
         positionSorted= sorted(enumerate(position), key=lambda i: i[1])
-       
+     
         for i, place in enumerate(positionSorted):
             
             time=float((target-place[1]))/float(speed[place[0]])
             
             
-            while(len(stack)>0 and stack[-1]<=time):
+            while stack and stack[-1]<=time:
                 stack.pop()
             stack.append(time)
         return(len(stack))
