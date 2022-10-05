4679f453308de02c4515044fc0c676197a980d1b
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-05 20:28:39 +0300

    Time: 59 ms (18.84%), Space: 13.5 MB (98.96%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -1,28 +1,19 @@
 class Solution(object):
     def reverseWords(self, s):
-     
-        arr =[]
-        word =[]
-        for i, char in enumerate(s):
-            if not word  and char == " ":
-                continue
+        output =""
+        i ,j = 0,0
+        while j < len(s):
+            while i < len(s) and s[i] == " ":
+                  i +=1
+            j = i + 1
+            while j < len(s) and s[j] != " ":
+                  j +=1
+            if i< len(s) and s[i] != " ":
+              output = s[i:j] + " " + output if output else s[i:j] + output
            
-            if char ==" ":
-                arr.append(word)
-                word = []
-            else:
-                word.append(char)
-       
-        if word:
-            arr.append(word)
-        l, r = 0, len(arr) - 1
-        while l < r:
-            arr[l] , arr[r] = arr[r], arr[l]
-            l +=1
-            r -=1
-      
-        return " ".join("".join(stri for stri in char) for char in arr)
-                
+            i = j +1
+        return output
+           
         """
         :type s: str
         :rtype: str
