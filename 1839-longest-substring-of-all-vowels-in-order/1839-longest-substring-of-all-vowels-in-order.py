807f0e0a21052a716aeaf95b6f63e2c0d072f4c3
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-07 11:39:02 +0300

    Time: 724 ms (81.82%), Space: 22.5 MB (31.82%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def longestBeautifulSubstring(self, word):
-          count, l, maxlen=0, -1, 0
+          count, l, maxlen=0, 0, 0
           vowel =set()
           for index, vow in enumerate(word):
                 if index >0 and vow < word[index -1]:
                     vowel = set()
-                    l = index - 1
+                    l = index 
                 vowel.add(vow)
                 if len(vowel) == 5 :
-                    maxlen = max (maxlen, index - l )
+                    maxlen = max (maxlen, index - l +1 )
           return maxlen
                     
             
