27cf086d54556378596b220199d6a5c797260bbc
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-07 11:37:58 +0300

    Time: 726 ms (77.27%), Space: 22.3 MB (81.82%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -1,38 +1,16 @@
 class Solution(object):
     def longestBeautifulSubstring(self, word):
-        vowel ={"a":1, "e":2 , "i":3, "o":4 , "u":5}
-        count, maxlen =1,0
-        l, r =0,0
-        while r < len(word):
-          
-            if l==0 and word[r] == "a" and word[l] !="a":
-              
-                   l = r
-            elif l == 0 and word[l] != "a":
-                r +=1
-                continue
-            if vowel[word[r-1]] == vowel[word[r]] - 1:
-                    count +=1
-            elif (vowel[word[r-1]] != vowel[word[r]] - 1  and word[r-1] != word[r]) :
-                if count ==  5:
-                       maxlen = max(maxlen, r - l )
-                l = r
-                while l<len(word) and word[l] != "a":
-                    l +=1
-                r = l + 1
-                if r<len(word)-3:
-                    count = 1
-                continue
-            r += 1
-        
-        if count ==  5:
-                       maxlen = max(maxlen, r - l )
-        return maxlen
+          count, l, maxlen=0, -1, 0
+          vowel =set()
+          for index, vow in enumerate(word):
+                if index >0 and vow < word[index -1]:
+                    vowel = set()
+                    l = index - 1
+                vowel.add(vow)
+                if len(vowel) == 5 :
+                    maxlen = max (maxlen, index - l )
+          return maxlen
                     
             
-                    
-        """
-        :type word: str
-        :rtype: int
-        """
+     
         
