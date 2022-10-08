270b2ea068e72dd4da38fa3040e95cc80bb43032
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-08 22:28:56 +0300

    Time: 5999 ms (7.70%), Space: 13.2 MB (98.90%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,16 +1,5 @@
 class Solution(object):
     def subArrayRanges(self, nums):
-        inc_mono =[]
-        dec_mono =[]
-        def inc (num):
-            while inc_mono and inc_mono[-1] > num:
-                inc_mono.pop()
-            inc_mono.append(num)
-        def dec (num):
-          
-            while dec_mono and dec_mono[-1] < num:
-                dec_mono.pop()
-            dec_mono.append(num)
         summed =0
         for i in range(len(nums)):
              maxi =-float("inf")
@@ -18,7 +7,6 @@ class Solution(object):
              for j in range(i, len(nums)):
                 mini =min(mini, nums[j])
                 maxi =max(maxi, nums[j])
-                
                 summed += (maxi - mini)
               
              
