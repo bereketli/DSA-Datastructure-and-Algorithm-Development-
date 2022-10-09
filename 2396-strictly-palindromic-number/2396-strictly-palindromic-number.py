d5f8a47b220163bd8d22833a6b0d8305bd0ac08f
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-09 22:07:37 +0300

    Time: 24 ms (78.45%), Space: 13.6 MB (48.62%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        if n == 4:
+            return False
+        remain =""
+        remain += str(n % (n - 2) )
+        
+        remain =str(int(n / (n-2)) % (n - 2)) + remain
+        
+        if remain != "12":
+            return True
+        else:
+            return False
+            
+      
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
