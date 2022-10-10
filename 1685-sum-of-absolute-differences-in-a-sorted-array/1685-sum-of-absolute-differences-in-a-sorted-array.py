845520d689cb7cc43de58bae099dbab84d7684b2
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-10 11:12:45 +0300

    Time: 2464 ms (5.13%), Space: 28.9 MB (30.77%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,15 +1,14 @@
 class Solution(object):
     def getSumAbsoluteDifferences(self, nums):
-        prefix=[nums[0]]
-        result=[]
-        for i in range(1,len(nums)):
-            
-            prefix.append(prefix[i - 1] + nums[i])
+        sum_T =sum(nums)
+        prefix =0
+        result =[]
         for i in range(len(nums)):
+            prefix += nums[i]
             
-            sumgreater = (prefix[len(nums) - 1] - prefix[i]) - (nums[i] *(len(nums) - i -1))
+            sumgreater = (sum_T - prefix) - (nums[i] *(len(nums) - 1 -i))
             
-            sumsmaller = (((i+1) * nums[i]) - prefix[i])  if i >0  else 0
+            sumsmaller = (((i+1) * nums[i]) - prefix)  
             result.append(sumgreater + sumsmaller)
         return result
             
