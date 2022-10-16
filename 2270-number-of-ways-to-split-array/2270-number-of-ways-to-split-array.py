193f29e75dab2c406ad793892b5b87e327bbf4a2
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-16 14:47:08 +0300

    Time: 1171 ms (80.72%), Space: 25.5 MB (62.65%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -1,15 +1,10 @@
 class Solution(object):
     def waysToSplitArray(self, nums):
-        post =[0] * len(nums)
+        summed =sum(nums)
         pref,count =0,0
-        for i in range(len(nums) - 1, -1, -1):
-             if i == len(nums) -1 :
-                post[len(nums) -1] = nums[len(nums) -1]
-             else:
-                post[i] =nums[i] + post[i + 1]
         for i in range(len(nums) - 1):
             pref +=nums[i]
-            if pref >= post[i +1]:
+            if pref >= summed - pref:
                 count +=1
         return count
             
