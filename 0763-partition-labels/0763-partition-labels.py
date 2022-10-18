22a5c1c4cfd5962700cc2f19155f91404d1676a7
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-18 13:34:22 +0300

    Time: 54 ms (42.72%), Space: 13.5 MB (63.34%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,24 +1,17 @@
 class Solution(object):
     def partitionLabels(self, s):
-        l= 0
-        prefi =0
-        output =[]
-        counted =Counter(s)
-        for r  in range(len(s)):
-            counted[s[r]] -=1
-            while counted[s[l] ]==0 and counted[s[r]] ==0 and l < r:
-                l +=1
-            if l ==r  and counted[s[l] ]==0 and counted[s[r]] ==0:
+       dic ={}
+       for i in range(len(s)):
+            dic[s[i] ] =i
+       count ,output=0, []
+       maxi =0
+       for i in range(len(s)):
+            maxi = max(maxi, dic[s[i]])
+            count +=1
+            if i == maxi:
+                output.append(count)
+                count =0
+       return output
                 
-                result = r +1 if not output else r - prefi +1
                 
-                output.append(result)
-                prefi += output[-1]
-        return output
-                
-            
-        """
-        :type s: str
-        :rtype: List[int]
-        """
-        
+       
