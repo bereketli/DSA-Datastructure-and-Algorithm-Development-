26f72e46cc16ae314c9624f0751413aa4acc1b00
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2022-10-26 16:31:49 +0300

    Time: 217 ms (54.62%), Space: 14.4 MB (70.64%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,37 +1,52 @@
 class Solution(object):
     def findAnagrams(self, s, p):
         dic1 ={}
-        for i in p:
-            if i not in dic1:
-                dic1[i] =1
-            else:
-                dic1[i] +=1
-        l =0
         output =[]
-        dic2 ={}
-        lenp = len(p)
-        for r in range(len(s)):
-            if s[r] not in dic2:
-                dic2[s[r]]  = 1
+        for i in range(len(p)):
+            if p[i] not in dic1:
+                dic1[p[i]] =1
             else:
-                dic2[s[r]] +=1       
-                
-                
-                
-            if r - l +1 < lenp:
-                continue
-            elif r - l +1 >lenp:
-                dic2[s[l]] -=1
-                if dic2[s[l]] == 0:
+                dic1[p[i]] +=1
+        dic2 ={}
+        l =0
+        for r in range(len(s)):
+             if s[r] not in dic2:
+                    dic2[s[r]] =1
+             else:
+                    dic2[s[r]] +=1
+             
+             if r >len(p) -1:
+                if dic2[s[l]] == 1:
                     del dic2[s[l]]
+                else:
+                    dic2[s[l]] -=1
                 l +=1
-            if dic2 ==dic1:
+            
+             if dic1 == dic2:
                 output.append(l)
         return output
+                    
+                
+       
             
              
             
         """
+        s = "cbaebabacd", p = "abc"
+        dic1 ={
+        a:1
+        b:1
+        c:1
+        }
+        l =1
+        r =2
+        dic2{
+        
+        b:1
+        a:1
+        e:1
+        }
+        
         :type s: str
         :type p: str
         :rtype: List[int]
