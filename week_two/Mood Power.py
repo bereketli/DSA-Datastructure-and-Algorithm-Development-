313cc274b170791e15a6bdad43b9f7f0fbe4a67c
berekt <blingerew2018@gmail.com>
2022-12-30 17:30:07 +0300

    first commit

---
+++ week_two/Mood Power.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to 
+import math
+a = int(input())
+b = int(input())
+m = int(input())
+son =int(math.pow(a,b))
+print(son)
+print(son % m)
