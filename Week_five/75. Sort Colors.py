3eb02b8d5752eca905daee0ea03e048087c50cdb
berekt <blingerew2018@gmail.com>
2023-01-20 22:14:09 +0300

    Space: O(1) Time: O(n)

---
+++ Week_five/75. Sort Colors.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+      count = [0] * 3
+      for num in nums:
+          count[num] +=1
+      output =0
+      for i, value in enumerate(count):
+          output += value
+        
+          for j in range(output - value, output):
+              nums[j] = i
+      
+      return output
+       
+
+              
+       
+          
