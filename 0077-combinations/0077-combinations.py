4fb9169562cf3a6926ee8da00a92fbf1f2718201
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2023-03-10 15:20:34 +0300

    Time: 87 ms (90.90%), Space: 15.8 MB (82.97%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -4,10 +4,9 @@ class Solution(object):
         self.sln = []
         self.size = n
         def combination(index, arr):
-           
             i = index
-            for i in range(index, (self.size - self.k )+ 2+ len(arr)):
-                arr.append(i)
+            for i in range(index, (self.size - (self.k - 1)) + 1 + len(arr)):
+                arr.append(i) 
                 if len(arr) < self.k:
                    combination(i + 1, arr)
                 elif len(arr) == self.k:
