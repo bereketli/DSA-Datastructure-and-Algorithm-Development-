eaf879a48d03ee345c5e76f0c3812a87f10c0386
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2023-04-06 17:33:17 +0300

    Backtracking

---
+++ Camp_progress/77. Combinations.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def combine(self, n, k):
+        self.k = k
+        self.sln = []
+        self.size = n
+        def combination(index, arr):
+            i = index
+            for i in range(index, (self.size - (self.k - 1)) + 1 + len(arr)):
+                arr.append(i) 
+                if len(arr) < self.k:
+                    combination(i + 1, arr)
+                elif len(arr) == self.k:
+                    new = [] + arr
+                    self.sln.append(new)
+                 
+                arr.pop()
+                
+            return
+            
+        
+        
+        combination(1,[])
+       
+        return self.sln
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        
