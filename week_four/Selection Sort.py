4702edfa1ef13ddc4058f1afb22cd2c182214234
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2023-04-12 23:39:37 +0300

    Time:O(n^2)   Space:O(1)

---
+++ week_four/Selection Sort.py
@@ -0,0 +1,25 @@
+
+class Solution: 
+    def select(self, arr, i):
+        minimum = i-1
+        for j in range(i,len(arr)):
+            if arr[minimum] > arr[j]:
+               minimum = j
+        return minimum
+        
+        # code here 
+    
+    def selectionSort(self, arr,n):
+        self.arr = arr
+        for i in range(n):
+            
+            minimum=self.select(self.arr ,i+1)
+           
+            
+            if minimum != i:
+               self.arr[i],  self.arr[minimum] = self.arr[minimum], self.arr[i]
+               
+            
+        return self.arr
+            
+        #code here
