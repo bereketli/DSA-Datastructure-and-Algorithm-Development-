f19e4fce8064e3dc5001c3a676dadc358e678bb8
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2023-04-12 22:50:14 +0300

    Time:O(n^2)  Space:O(1)

---
+++ week_four/969. Pancake Sorting.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        output =[]
+        def fliper(index):
+            l =0
+            while l < index:
+                arr[l], arr[index] =arr[index], arr[l]
+                l +=1
+                index -=1
+        index= len(arr) - 1
+        while index > 0:
+            max_i =0
+            for i in range(1,index + 1):
+                if arr[max_i] < arr[i]:
+                    max_i = i
+            fliper(max_i)
+            output.append(max_i + 1)
+            fliper(index)
+            output.append(index + 1)
+            index -=1
+        return output
