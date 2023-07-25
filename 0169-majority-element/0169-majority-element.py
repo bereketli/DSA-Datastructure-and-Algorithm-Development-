6c5f2e3256f1f88306cfffed1f57835140ecb3d1
bereketli <blingerew2018@gmail.com>
2023-07-25 21:09:59 +0300

    remove_separated

--- 0169-majority-element/0169-majority-element.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        counted = Counter(nums)
-        for ky in counted:
-            if counted[ky] > len(nums)/2:
-                return ky
