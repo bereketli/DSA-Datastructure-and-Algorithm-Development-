318e5bbdc8a7fac919907f1a5367456baa6159e2
Bereket Lingerew <95904361+bereketli@users.noreply.github.com>
2023-11-30 23:58:50 +0300

    Added solution for Find Peak Element

---
+++ After-First-Year/find-peak-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l, r = 0, n - 1
+        while l < r:
+            mid = (l + r) >> 1
+            if nums[mid] < nums[mid + 1]:
+                l =  mid + 1
+            else:
+                r = mid 
+        return l
